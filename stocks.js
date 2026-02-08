15e6326f7651f1bee7796cad11dde555858dec70
Geo <greektrading01@gmail.com>
2026-02-08 13:08:20 +0200

    limit to SP500 + NASDAQ100

--- stocks.js
+++ stocks.js
@@ -1,19 +1,31 @@
 const axios = require("axios");
 
+const FINNHUB_API = process.env.FINNHUB_API;
+
+// ---- S&P500 symbols ----
+const SP500_URL =
+  "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents_symbols.txt";
+
+// ---- NASDAQ100 symbols ----
+const NASDAQ100_URL =
+  "https://raw.githubusercontent.com/pssolanki111/NASDAQ-100-Stock-Tickers/main/nasdaq100_tickers.txt";
+
 async function getAllStocks() {
   try {
-    const res = await axios.get(
-      `https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${process.env.FINNHUB_API}`
-    );
+    const sp = await axios.get(SP500_URL);
+    const nasdaq = await axios.get(NASDAQ100_URL);
 
-    const all = res.data;
+    const sp500 = sp.data.split("\n");
+    const nasdaq100 = nasdaq.data.split("\n");
 
-    const filtered = all
-      .filter(s => s.type === "Common Stock")
-      .map(s => s.symbol);
+    // merge + remove duplicates
+    const merged = [...new Set([...sp500, ...nasdaq100])];
 
-    return filtered;
+    console.log("S&P500:", sp500.length);
+    console.log("NASDAQ100:", nasdaq100.length);
+    console.log("Total tradable stocks:", merged.length);
 
+    return merged.filter(Boolean);
   } catch (err) {
     console.log("Stock list error:", err.message);
     return [];
