e00f50b6ff50345b2caa239046cbfdbb7ce9eb75
Geo <greektrading01@gmail.com>
2026-02-08 02:04:59 +0200

    trim token

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const token = process.env.taken;
+const token = process.env.token?.trim();
 const {Client, GatewayIntentBits} = require("discord.js");
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages]});
