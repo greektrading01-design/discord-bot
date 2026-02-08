a03f5d9576c3719d9c66e9c92ba8cd1a0394c0a4
Geo <greektrading01@gmail.com>
2026-02-08 02:07:37 +0200

    use TOKEN env

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const token = process.env.token?.trim();
+const token = process.env.TOKEN?.trim();
 const {Client, GatewayIntentBits} = require("discord.js");
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages]});
